ebde8ce4a1871e75d40849a34adfc5b2ac12a5d8
Phéna Proxima <adam@phenaproxima.net>
2021-06-15 14:00:57 -0400

    Document another one

--- fixtures/TUFTestFixtureThresholdTwo/__init__.py
+++ fixtures/TUFTestFixtureThresholdTwo/__init__.py
@@ -1,3 +1,10 @@
+"""
+A fixture with TUF metadata that requires at least two valid signatures.
+
+Expected Outcome: TUF clients should be able to successfully download and
+validate all targets in this fixture.
+"""
+
 from fixtures.builder import FixtureBuilder
 
 
